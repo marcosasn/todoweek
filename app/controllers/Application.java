072851fa9293ad58fa927e94f8cf99e9cffe0c26
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2014-11-21 14:30:38 -0300

    Commit inicial.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is read."));
+    }
+    
+    public static Result helloWord() {
+    	return ok("Hello Word!");
     }
 
 }
