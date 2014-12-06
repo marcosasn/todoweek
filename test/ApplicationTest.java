45e65d1decdfe42ac0fb4034c446f545aaf9ef00
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2014-12-06 15:54:17 -0300

    Incrementando os testes de Aplicação e ToDoWeek.

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,54 +1,194 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
 import models.Objetivo;
-
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-import play.twirl.api.Content;
+import models.Prioridade;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
+import models.dao.GenericDAO;
+import org.junit.Test;
+import play.mvc.Http;
+import play.mvc.Result;
 
-/**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
-public class ApplicationTest {
-	
-	private List<Objetivo> objetivos;
-	
-	@Before
-	public void setUp() {
-		objetivos = new ArrayList<Objetivo>();
-	}
+public class ApplicationTest extends AbstractTest {
+    
+	Result result;
 
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void deveConectarComIndex() throws Exception {
+        result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+        assertThat(redirectLocation(result)).isEqualTo(null);
     }
 
     @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render(objetivos);
-        assertThat(contentType(html)).isNotNull();
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("To Do Week");
+    public void deveMostrarInterfaceInicial() throws Exception {
+        result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+        assertThat(redirectLocation(result)).isEqualTo(null);
+        assertThat(charset(result)).isEqualTo("utf-8");
+
+       
+        String[] words = {"To Do Week", "Metas semana 1", "Metas semana 2", "Metas semana 3","Metas semana 4","Metas semana 5",
+        "Metas semana 6", "Criar meta","Semana 1","Semana 2","Semana 3","Semana 4","Semana 5","Semana 6"};
+		String html = contentAsString(result);
+		
+		for (String word : words) {
+			assertThat(html).contains(word);	
+		}
+	}
+
+    @Test
+    public void deveAdicionarNovoObjetivo() throws Exception {
+        Map<String, String> formData = new HashMap<>();
+        formData.put("nome", "Teste1");
+        formData.put("descricao", "Teste1 Descrição");
+        formData.put("prioridade", "MEDIA");
+        formData.put("semana", "Semana 1");
+
+        result = callAction(controllers.routes.ref.Application.index());
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+        assertThat(redirectLocation(result)).isEqualTo(null);
+
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+
+        
+        GenericDAO dao = new GenericDAO();
+        List<Objetivo> objetivos = dao.findAllByClassName(Objetivo.class.getName());
+        assertThat(objetivos.size()).isEqualTo(1);
+        assertThat(objetivos.get(0).getNome()).isEqualTo("Teste1");
+        assertThat(objetivos.get(0).getDescricao()).isEqualTo("Teste1 Descrição");
+        assertThat(objetivos.get(0).getPrioridade()).isEqualTo(Prioridade.MEDIA);
+        assertThat(objetivos.get(0).getSemana()).isEqualTo(1);
+
+        result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+        assertThat(contentAsString(result)).contains("Teste1");
+        assertThat(contentAsString(result)).contains("Teste1 Descrição");
+        assertThat(contentAsString(result)).contains("MEDIA");
+    }
+
+    @Test
+    public void naoDeveAdicionarNovoObjetivoFormVazio() throws Exception {
+        Map<String, String> formData = new HashMap<>();
+
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.BAD_REQUEST);
+
+        GenericDAO dao = new GenericDAO();
+        List<Objetivo> objetivos = dao.findAllByClassName(Objetivo.class.getName());
+        assertThat(objetivos).isEmpty();
+    }
+
+    @Test
+    public void deveOrdemPorSemana() throws Exception {
+    	Map<String, String> formData = new HashMap<>();
+        formData.put("nome", "Teste1");
+        formData.put("descricao", "Teste1 Descrição");
+        formData.put("prioridade", "MEDIA");
+        formData.put("semana", "Semana 1");
+        
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+        
+        formData.clear();
+        formData.put("nome", "Teste2");
+        formData.put("descricao", "Teste2 inferior a 1");
+        formData.put("prioridade", "MEDIA");
+        formData.put("semana", "Semana 2");
+
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+   
+        result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+        assertThat(redirectLocation(result)).isEqualTo(null);
+   
+        String html = contentAsString(result);
+        assertThat(html.indexOf("Teste1")).isLessThan(html.indexOf("Teste2"));
+    }
+    
+    public void deveOrdenarPorPrioridade() {
+    	Map<String, String> formData = new HashMap<>();
+        formData.put("nome", "Teste1");
+        formData.put("descricao", "Teste1 Descrição");
+        formData.put("prioridade", "MEDIA");
+        formData.put("semana", "Semana 1");
+ 
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+       
+        formData.clear();
+        formData.put("title", "Teste3");
+        formData.put("description", "Teste3 mais importante");
+        formData.put("priority", "Alta");
+        formData.put("week", "1");
+        
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+ 
+        result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+
+        String html = contentAsString(result);
+        assertThat(html.indexOf("Teste3")).isLessThan(html.indexOf("Teste1"));
+    }
+   
+    @Test
+    public void deveMudarObjetivoParafeito() throws Exception {
+    	Map<String, String> formData = new HashMap<>();
+        formData.put("nome", "Teste1");
+        formData.put("descricao", "Teste1 Descrição");
+        formData.put("prioridade", "MEDIA");
+        formData.put("semana", "Semana 1");
+
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+
+        result = callAction(controllers.routes.ref.Application.marcarMetaFeita(1L), fakeRequest());
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+
+        result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+        assertThat(contentAsString(result)).contains("FEITO");
+        
+        GenericDAO dao = new GenericDAO();
+        List<Objetivo> objetivos = dao.findAllByClassName(Objetivo.class.getName());
+        assertThat(objetivos.get(0).getStatus()).isEqualTo("FEITO");
+    }
+
+    @Test
+    public void deveDeletarObjetivo() throws Exception {
+    	Map<String, String> formData = new HashMap<>();
+        formData.put("nome", "Teste1");
+        formData.put("descricao", "Teste1 Descrição");
+        formData.put("prioridade", "MEDIA");
+        formData.put("semana", "Semana 1");
+
+        result = callAction(controllers.routes.ref.Application.criarMeta(),fakeRequest().withFormUrlEncodedBody(formData));
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+
+        result = callAction(controllers.routes.ref.Application.deletarMeta(1L), fakeRequest());
+        assertThat(status(result)).isEqualTo(Http.Status.SEE_OTHER);
+        assertThat(redirectLocation(result)).isEqualTo("/");
+
+        assertThat(contentAsString(result)).doesNotContain("Teste1");
+        assertThat(contentAsString(result)).doesNotContain("Teste1 Descrição");
+        
+        GenericDAO dao = new GenericDAO();
+        List<Objetivo> objetivos = dao.findAllByClassName(Objetivo.class.getName());
+        assertThat(objetivos).isEmpty();
     }
 }
--- test/ToDoWeekTest.java
+++ test/ToDoWeekTest.java
@@ -1,42 +1,11 @@
-import static org.fest.assertions.Assertions.assertThat;
 import static org.junit.Assert.*;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.HTMLUNIT;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.inMemoryDatabase;
-import static play.test.Helpers.running;
-import static play.test.Helpers.status;
-import static play.test.Helpers.testServer;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.persistence.EntityManager;
-import javax.swing.text.View;
-
 import models.Objetivo;
-import models.Prioridade;
 import models.ToDoWeek;
 import models.dao.GenericDAO;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.After;
-
-import play.GlobalSettings;
-import play.db.jpa.JPA;
-import play.db.jpa.JPAPlugin;
-import play.libs.F.Callback;
-import play.mvc.Http;
-import play.mvc.Result;
-import play.test.FakeApplication;
-import play.test.FakeRequest;
-import play.test.Helpers;
-import play.test.TestBrowser;
-import scala.Option;
-import views.html.defaultpages.badRequest;
-import views.html.defaultpages.todo;
 
 /**
  * Created by Marcos on 14/11/14.
@@ -46,37 +15,15 @@ public class ToDoWeekTest extends AbstractTest {
 
 	private GenericDAO dao = new GenericDAO();
 	private ToDoWeek todoweek =  new ToDoWeek();
-	//private Result result;
 	private List<Objetivo> objetivos;
-
-    
-    /*
-     * Como usuário, visito a página principa do sistema e vlejo uma lista das metas que tenho, para que eu saiba quais as
-     * próximas que tenho a perseguir. As metas estão ordenadas pela semana em que planejei cumpri-las, e, em seguida, por prioridade. 
-     * Para cada semana, o sistema diz quantas metas há planejadas naquela semana.
-     * 
-     * Como usuário, marco uma meta de minha lista como alcançada, e o sistema a destaca de maneira diferente das não alcançadas, 
-     * para que assim eu acompanhe meu progresso alcançando as metas em geral. A lista de metas deve agora mostrar para cada semana 
-     * o total de metas, o total de metas alcançadas e o total das a alcançar.
-     * 
-     * Como usuário, apago uma meta, para que assim eu remova do sistema metas que desisti de alcançar.
-     */
 	
-    /*VERIFICAR!!!!!!!!!!!!!
-    * Seu sistema deve já iniciar contendo 10 metas cadastradas para as próximas 3 semanas. 
-    * A forma fácil de fazer isso é usando o Global.java.
-    */
-    @Test
+	@Test
     public void deveIniciarSemObjetivos() throws Exception {
         objetivos = dao.findAllByClassName(Objetivo.class.getName());
         assertTrue(objetivos.isEmpty());
     }
 
-    /*
-     * Como usuário, cadastro uma nova meta, informando uma descrição, prioridade e escolhendo uma semana no futuro na qual
-     * desejo que tenha alcançado essa meta, para que assim minha lista de metas possa ser populada.
-     */
-    @Test
+	@Test
     public void deveSalvarObjetivoNoBD() throws Exception {
         dao.persist(todoweek.criarMeta("Pagar SI1", "Alta", "Quero pagar a disciplina de SI1.", "Semana 6"));
 
@@ -86,9 +33,6 @@ public class ToDoWeekTest extends AbstractTest {
         assertEquals(objetivos.get(0),todoweek.criarMeta("Pagar SI1", "Alta", "Quero pagar a disciplina de SI1.", "Semana 6"));
     }
     
-    /*
-     * Metas podem ser adicionadas para as próximas 6 semanas apenas.
-     */
     @Test
     public void deveSalvarObjetivosApenasParaProximasSeisSemanas() throws Exception {
     	try {
